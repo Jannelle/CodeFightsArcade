191e12856de1596915e91562cbb55304b7628477
Jannelle Zapanta <JannelleZapanta@gmail.com>
2017-05-22 15:45:09 -0700

    converted haunted rooms to 0; added sum

--- matrixElementsSum.py
+++ matrixElementsSum.py
@@ -7,22 +7,17 @@ def matrixElementsSum(matrix):
         i = 0
         for element in row:
 
-            print element, matrix[j][i]
+            if matrix[j][i] == 0:
+                matrix[j + 1][i] = 0
             i = i + 1
 
         j += 1
 
-    # loop through each element in the matrix
-    # if the element at the same index from the previous row is 0, make this element 0
-    # get the sum of each row
+    print sum([sum(l) for l in matrix])
 
 
-# matrix = [[0, 1, 1, 2],
-#           [0, 5, 0, 0],
-#           [2, 0, 3, 3]]
-
-matrix = [['1_1', '1_2', '1_3', '1_4'],
-          ['2_1', '2_2', '2_3', '2_4'],
-          ['3_1', '3_2', '3_3', '3_4']]
+matrix = [[0, 1, 1, 2],
+          [0, 5, 0, 0],
+          [2, 0, 3, 3]]
 
 print matrixElementsSum(matrix)
---
+++ try.py
@@ -0,0 +1,2 @@
+x = [[1, 2], [1, 2]]
+print sum([sum(l) for (l) in x])
