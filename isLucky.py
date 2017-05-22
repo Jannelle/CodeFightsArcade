a7c78c741e0985b31172e025f86cb105480079a0
Jannelle Zapanta <JannelleZapanta@gmail.com>
2017-05-22 16:44:45 -0700

    added isLucky get each half of the ticket

---
+++ isLucky.py
@@ -0,0 +1,10 @@
+def isLucky(n):
+
+    ticket_list = [int(n) for n in str(n)]
+    half = len(ticket_list) / 2
+    first_half = ticket_list[:half]
+    second_half = ticket_list[half:]
+
+
+n = 123456
+print isLucky(n)
