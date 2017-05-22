b388e51ce3fe2a6cfe6df248e62ceb5d40019133
Jannelle Zapanta <JannelleZapanta@gmail.com>
2017-05-22 16:01:42 -0700

    added line to get longest string

--- all_longest_strings.py
+++ all_longest_strings.py
@@ -1,5 +1,10 @@
 def allLongestStrings(inputArray):
-    pass
+    # find the longest string
+    longest_string = max(len(x) for x in array)
+    print longest_string
+
+    # loop through each string in the array
+        # if the string's length matches the longest string, print it
 
 array = ["aba", "aa", "ad", "vcd", "aba"]
 print allLongestStrings(array)
