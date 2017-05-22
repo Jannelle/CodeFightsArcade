4130c9d505bb468f56e69fa0d807fdfe451c9fe3
Jannelle Zapanta <JannelleZapanta@gmail.com>
2017-05-22 16:38:01 -0700

    completed commonCharacterCount

--- common_character_count.py
+++ common_character_count.py
@@ -1,2 +1,10 @@
 def commonCharacterCount(s1, s2):
-    pass
+
+    # Get a list of individual characters is in s1
+    unique_characters = list(set(s1))
+
+    common_count = 0
+    for character in unique_characters:
+        common_count += min(s1.count(character), s2.count(character))
+
+    return common_count
