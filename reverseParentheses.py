44aa8bfdfb284cdbeccf403918d49f4aed9dae0b
Jannelle Zapanta <JannelleZapanta@gmail.com>
2017-05-22 18:08:59 -0700

    make a loop for each parenthetical

--- reverseParentheses.py
+++ reverseParentheses.py
@@ -2,12 +2,13 @@ import re
 
 def reverseParentheses(s):
 
-    # make a lsit of all open parenthesis indices
-    open_indices = []
-    for match in re.finditer('\(', ab):
-        open_indices.append(match.start())
+    # make a list of all open parenthesis indices
+    open_indices = sorted(get_indices('\(', s), reverse=True)
+    close_indices = get_indices('\)', s)
 
-    print open_indices
+    for i in range(0, len(open_indices)):
+        print s[open_indices[i] + 1:close_indices[i]]
+
     # loop this for each parenthetical:
         # get the innermost parenthetical
             # the innermost is the open parenthesis with the largest index
@@ -17,6 +18,14 @@ def reverseParentheses(s):
                     # reverse it
         # get the next parenthetical (outward)
 
+
+def get_indices(delimiter, str):
+
+    indices_list = []
+    for match in re.finditer(delimiter, str):
+        indices_list.append(match.start())
+
+    return indices_list
 
 ab = 'hi (jann(el)le) bye'
 reverseParentheses(ab)
