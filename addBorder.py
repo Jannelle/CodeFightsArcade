46b41086eeb3162f4bb78ef51ad887ae370c0a7a
Jannelle Zapanta <JannelleZapanta@gmail.com>
2017-05-23 02:05:13 -0700

    submitted addBorder

--- addBorder.py
+++ addBorder.py
@@ -1,18 +1,44 @@
+'''
+Given a rectangular matrix of characters, add a border of asterisks(*) to it.
+
+Example
+
+For
+
+picture = ["abc",
+           "ded"]
+the output should be
+
+addBorder(picture) = ["*****",
+                      "*abc*",
+                      "*ded*",
+                      "*****"]
+'''
+
 def addBorder(picture):
-    # add the top layer
-    add_layer(0, picture)
+
+    star_count = len(picture[0])
+    add_layer(0, picture, star_count)
 
     # add left and right borders to each row
-    for row in picture:
-        add_border(row)
+    for i in range(1, len(picture)):
+        wrap_row(i, picture)
 
-    # add bottom layer
-    add_layer(len(picture) - 1, picture)
+    add_layer(len(picture), picture, star_count)
+
+    return picture
 
 
+def add_layer(row, picture, star_count):
 
-def add_top_layer(row, picture):
-    pass
+    picture.insert(row, (star_count + 2) * '*')
+    return picture
 
-def add_border(row, picture):
-    pass
+
+def wrap_row(row, picture):
+    picture[row] = "*" + picture[row] + "*"
+    return picture
+
+x = ['hi', 'to', 'hi']
+addBorder(x)
+print x
