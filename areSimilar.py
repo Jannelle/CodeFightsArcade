1ed31f33308bf2605b0044ece7e0485f47b66e6c
Jannelle Zapanta <JannelleZapanta@gmail.com>
2017-05-23 14:13:29 -0700

    Added checks for the same elements Added loop to collect mismatching elements Added check to term if there are too many mismatching indexes

--- areSimilar.py
+++ areSimilar.py
@@ -23,10 +23,28 @@ Any swap of any two elements either in a or in b won't make a and b equal.
 
 def areSimilar(a, b):
     # in order for two arrays to be similar, they must have the same elements
-    if not have_same_elements: return false
-    # check they have the same elements
-    # map each element to their index
-    # compare each element and their indexes between the two arrays
+    if sorted(a) != sorted(b):
+        return False
+
+    mismatching_a = []
+    mismatching_b = []
+
+    for i in range(0, len(a)):
+        if a[i] != b[i]:
+            mismatching_a.append((a[i], i))
+            mismatching_b.append((b[i], i))
+
     # if there are more than two mismatching index, they are not similar
+    if len(mismatching_a) > 2:
+        return False
+
+    print mismatching_a
+    print mismatching_b
+
+
     # if there are two mismatching indexes, see if they can be swapped
 
+
+a = [3, 2, 1]
+b = [1, 3, 2]
+print areSimilar(a, b)
