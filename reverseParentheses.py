0acd06359eb15409703ebcb8c2d14251fd48d950
Jannelle Zapanta <JannelleZapanta@gmail.com>
2017-05-22 17:56:08 -0700

    added pseudocode get the start of each (

--- reverseParentheses.py
+++ reverseParentheses.py
@@ -1,2 +1,20 @@
+import re
+
 def reverseParentheses(s):
-    pass
+
+    # loop this for each parenthetical:
+    for match in re.finditer('\(', ab)
+        print match.start()
+
+        # get the innermost parenthetical
+            # the innermost is the open parenthesis with the largest index
+                # find all open parenthesis indices
+                # make a list of these parenthesis
+                # find the largest value in this list
+                    # reverse it
+        # get the next parenthetical (outward)
+
+
+ab = 'hi (jann(el)le) bye'
+
+
