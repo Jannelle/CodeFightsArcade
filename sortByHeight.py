08216e0f0cd5eedcb09caa1f095cf2549369148b
Jannelle Zapanta <JannelleZapanta@gmail.com>
2017-05-22 17:15:20 -0700

    final version for sortByHeight

--- sortByHeight.py
+++ sortByHeight.py
@@ -2,9 +2,8 @@ def sortByHeight(a):
 
     tree_positions = get_tree_positions(a)
     remove_trees(tree_positions, a)
-    a.sort
-    reinsert_trees(tree_positions, a)
-    return a
+    a.sort()
+    return reinsert_trees(tree_positions, a)
 
 
 def get_tree_positions(a):
@@ -28,6 +27,4 @@ def reinsert_trees(treepositions, a):
     for i in treepositions:
         a.insert(i, -1)
 
-
-a = [-1, 150, 190, 170, -1, -1, 160, 180]
-print sortByHeight(a)
+    return a
