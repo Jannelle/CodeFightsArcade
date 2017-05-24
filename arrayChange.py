658cab6a9d23470d9ea7f32f79f40d26d10afb2b
Jannelle Zapanta <JannelleZapanta@gmail.com>
2017-05-24 16:21:25 -0700

    removed isIncreasing check

--- arrayChange.py
+++ arrayChange.py
@@ -12,17 +12,12 @@ def arrayChange(inputArray):
 
     count = 0
     # do this until it's strictly increasing
-    while not is_increasing(inputArray):
-        for i in range(0, len(inputArray) - 1):
-            while inputArray[i] >= inputArray[i + 1]:
-                inputArray[i + 1] += 1
-                count += 1
+    for i in range(0, len(inputArray) - 1):
+        while inputArray[i] >= inputArray[i + 1]:
+            inputArray[i + 1] += 1
+            count += 1
 
     return count
 
-def is_increasing(inputArray):
-    # must be sorted and have no duplicates
-    if inputArray == sorted(inputArray) and len(inputArray) == len(set(inputArray)):
-        return True
-    else:
-        return False
+x = [1, 6, 5]
+print arrayChange(x)
